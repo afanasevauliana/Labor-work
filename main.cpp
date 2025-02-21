b523c17b6f65c9aa2ab7ebb14320c9c998f8eab2
afanasevauliana <afanasevauliana946@gmail.com>
2025-02-21 13:58:44 +0300

    3

--- main.cpp
+++ main.cpp
@@ -1,12 +1,15 @@
 #include "menu/CMenu.h"
 #include "menu/CMenuItem.h"
+#include <cmath>
 
 using namespace std;
 
 
 #pragma region функции-заглушки
 int f1() {
+    int value = 25;
     std::cout << "function f1 is running...\n\n";
+    std::cout << sqrt(value);
     return 1;
 }
 
@@ -30,7 +33,7 @@ const int ITEMS_NUMBER = 4;
 
 
 int main() {
-    CMenuItem items[ITEMS_NUMBER] {CMenuItem{"first item", f1}, CMenuItem{"second item", f2}, CMenuItem{"third item", f3}, CMenuItem{"Fourth item",f4}};
+    CMenuItem items[ITEMS_NUMBER] {CMenuItem{"root of 25", f1}, CMenuItem{"second item", f2}, CMenuItem{"third item", f3}, CMenuItem{"Fourth item",f4}};
     CMenu menu("My console menu", items, ITEMS_NUMBER);
     while (menu.runCommand()) {};
 
