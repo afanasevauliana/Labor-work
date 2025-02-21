0cc332319fb25fc0249d9db5697caf6cf5488a88
afanasevauliana <afanasevauliana946@gmail.com>
2025-02-21 14:34:18 +0300

    4

--- main.cpp
+++ main.cpp
@@ -1,39 +1,36 @@
 #include "menu/CMenu.h"
 #include "menu/CMenuItem.h"
 #include <cmath>
-
+#include <limits>
 using namespace std;
-
-
 #pragma region функции-заглушки
+
 int f1() {
     int value = 25;
-    std::cout << "function f1 is running...\n\n";
-    std::cout << sqrt(value);
+    cout << sqrt(value) << endl;
     return 1;
 }
 
 int f2() {
-    std::cout << "function f2 is running...\n\n";
+    cout << "\n";
+    cout << "   *   \n";
+    cout << "  ***  \n";
+    cout << " ***** \n";
+    cout << "*******\n";
+    cout << "*     *\n";
+    cout << "*     *\n";
+    cout << "*     *\n";
+    cout << "*******\n";
     return 2;
 }
 
-int f3() {
-    std::cout << "function f3 is running...\n\n";
-    return 3;
-}
-
-int f4() {
-    std::cout << "function f4 is running...\n\n";
-    return 4;
-}
 #pragma endregion
 
-const int ITEMS_NUMBER = 4;
+const int ITEMS_NUMBER = 2;
 
 
 int main() {
-    CMenuItem items[ITEMS_NUMBER] {CMenuItem{"root of 25", f1}, CMenuItem{"second item", f2}, CMenuItem{"third item", f3}, CMenuItem{"Fourth item",f4}};
+    CMenuItem items[ITEMS_NUMBER] {CMenuItem{"root of 25", f1}, CMenuItem{"print house", f2}};
     CMenu menu("My console menu", items, ITEMS_NUMBER);
     while (menu.runCommand()) {};
 
