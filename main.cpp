bc1491b197beab40fd0feec2bc11856128bb4cd8
afanasevauliana <afanasevauliana946@gmail.com>
2025-02-21 10:40:00 +0300

    2

--- main.cpp
+++ main.cpp
@@ -19,13 +19,18 @@ int f3() {
     std::cout << "function f3 is running...\n\n";
     return 3;
 }
+
+int f4() {
+    std::cout << "function f4 is running...\n\n";
+    return 4;
+}
 #pragma endregion
 
-const int ITEMS_NUMBER = 3;
+const int ITEMS_NUMBER = 4;
 
 
 int main() {
-    CMenuItem items[ITEMS_NUMBER] {CMenuItem{"first item", f1}, CMenuItem{"second item", f2}, CMenuItem{"third item", f3}};
+    CMenuItem items[ITEMS_NUMBER] {CMenuItem{"first item", f1}, CMenuItem{"second item", f2}, CMenuItem{"third item", f3}, CMenuItem{"Fourth item",f4}};
     CMenu menu("My console menu", items, ITEMS_NUMBER);
     while (menu.runCommand()) {};
 
